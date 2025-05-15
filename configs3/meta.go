bd1e1b29b6cda7b051a7aa503b8d838e6ca36677
zack <zzhang_xz@163.com>
2025-05-15 15:51:22 +0800

    feat(s3): add meta struct for s3

---
+++ configs3/meta.go
@@ -0,0 +1,11 @@
+package configs3
+
+type Meta struct {
+	Width     float64 `json:"width"`
+	Height    float64 `json:"height"`
+	Size      float64 `json:"size"`
+	Aspect    float64 `json:"aspect"`
+	FrameRate float64 `json:"frame_rate"`
+	Bitrate   float64 `json:"bitrate"`
+	Duration  float64 `json:"duration"`
+}
